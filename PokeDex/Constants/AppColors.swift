af0c157f7aec596bdf86fd2014e0f9ae6f9a14fb
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-11-05 18:50:02 +0300

    Added Constant Classes

---
+++ PokeDex/Constants/AppColors.swift
@@ -0,0 +1,17 @@
+//
+//  AppColors.swift
+//  PokeDex
+//
+//  Created by Ali Çolak on 5.11.2023.
+//
+
+import UIKit
+
+struct AppColors {
+    
+    static let appRedColor = UIColor(hex: "#DC0A2D")
+    static let placeHolderColor  = UIColor(hex: "#666666")
+    static let blackColor = UIColor(hex: "#1D1D1D")
+    static let whiteColor = UIColor(hex: "#FFFFFF")
+    
+}
---
+++ PokeDex/Constants/AppTexts.swift
@@ -0,0 +1,12 @@
+//
+//  AppTexts.swift
+//  PokeDex
+//
+//  Created by Ali Çolak on 5.11.2023.
+//
+
+import Foundation
+
+struct AppTexts {
+    static let searchBarPlaceholderText = "Search"
+}
